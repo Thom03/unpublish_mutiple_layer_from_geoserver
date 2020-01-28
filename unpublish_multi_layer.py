e7d579e1ae7b0d8025898fc6a65a0feeeee06316
thom03 <thomas.muteti@gmail.com>
2020-01-28 19:59:43 +0300

    Added License file.

--- unpublish_multi_layer.py
+++ unpublish_multi_layer.py
@@ -1,1 +1,8 @@
 from geoserver.catalog import Catalog
+
+"""To Establish connection to the catalog"""
+conn = Catalog("http://localhost:8080/geoserver/rest", username="admin", password="geoserver")
+
+print(conn)
+
+# all_layers = conn.get_layers()
